4d098c7e417564a09c251dc40eb672fa16e9b0db
Kenny Chang <yo8568@gmail.com>
2018-06-29 17:08:58 +0800

    fix: fix lifes end is love typo

--- quotes/lifes-end-is-love.js
+++ quotes/lifes-end-is-love.js
@@ -1,5 +1,6 @@
 /**
  * Life's end is love
+ * 
  * 人生就是必須經歷到一些障礙
  * 從障礙中丟掉世俗的假設
  * 最後, 便擁有了愛
@@ -7,7 +8,7 @@
  * ----工程師午後浪漫 少東筆
  */
 
-exprot.module = function () {
+module.exports = function () {
   return ['life']
     .reduce((experience, trouble) =>
       experience.concat(
